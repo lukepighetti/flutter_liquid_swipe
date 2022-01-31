9d5807e7792539d25db9ca5ae05f9c53c95ba79a
Luke Pighetti <lukepighetti@gmail.com>
2022-01-30 20:29:43 -0500

    fix theme color

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'liquid_swipe',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: _HomePage(),
